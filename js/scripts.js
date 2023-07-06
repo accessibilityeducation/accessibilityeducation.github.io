0e2b3d8aa0896a1d4f05702ee62ed69e78e1189c
mondiriariana <arianamondiri@gmail.com>
2023-07-06 14:11:12 -0500

    added ressources page

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,9 @@ window.addEventListener('DOMContentLoaded', () => {
     let scrollPos = 0;
     const mainNav = document.getElementById('mainNav');
     const headerHeight = mainNav.clientHeight;
-    window.addEventListener('scroll', function() {
+    window.addEventListener('scroll', function () {
         const currentTop = document.body.getBoundingClientRect().top * -1;
-        if ( currentTop < scrollPos) {
+        if (currentTop < scrollPos) {
             // Scrolling Up
             if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
                 mainNav.classList.add('is-visible');
@@ -27,3 +27,31 @@ window.addEventListener('DOMContentLoaded', () => {
         scrollPos = currentTop;
     });
 })
+
+
+/* for dynamic filter section*/
+var LOButton = document.querySelectorAll("input[id*='checkLO']");
+for (let i = 0; i <= LOButton.length; i++) {
+    LOButton[i].addEventListener("click", () => {
+        var labelName = "label-" + LOButton[i].getAttribute("id");
+        var inputName = "input-" + LOButton[i].getAttribute("id");
+        var filterLabels = document.querySelectorAll("label[name*=" + CSS.escape(labelName));
+        var filterInputs = document.querySelectorAll("input[name*=" + CSS.escape(inputName));
+        if (LOButton[i].checked) {
+            document.getElementById("filter-section").style.display= "inline-block";
+            for (let k = 0; k < filterLabels.length; k++) {
+                filterLabels[k].style.display = "inline-block";
+            }
+        }
+        else {
+            document.getElementById("filter-section").style.display= "none";
+                for (let l = 0; l < filterInputs.length; l++) {
+                      (filterInputs[l]).checked = false;
+                      filterLabels[l].style.display = "none";
+                     }
+                 
+        }
+    }
+    )
+}
+
